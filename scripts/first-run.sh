0c011b6ea21b1966fb316c6128bead0728bc0fa2
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-02-16 16:26:45 +0100

    Delete first-run.sh

--- scripts/first-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Install docker and docker-compose here. Can't currently install in the APK :/
-sudo pkg install -y docker docker-compose
-
-# Generate keys for SSH
-ssh-keygen -A
