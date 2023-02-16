050518187c59c9ba020baedac065b081fc42b923
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-02-16 16:28:07 +0100

    Install tini from git source

--- scripts/build-tini.sh
+++ scripts/build-tini.sh
@@ -3,9 +3,8 @@ pkg install golang make cmake ndk-multilib
 mkdir $TMPDIR/docker-build
 cd $TMPDIR/docker-build
 cd $TMPDIR/docker-build
-wget https://github.com/krallin/tini/archive/v0.19.0.tar.gz
-tar xf v0.19.0.tar.gz
-cd tini-0.19.0
+git clone --depth 1 https://github.com/krallin/tini
+cd tini
 mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
