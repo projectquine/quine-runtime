c68d988b818536f76de2fa58fb47615c147266f1
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-03-15 14:17:11 +0100

    Dont hardcode permissions change on /dev/tty

--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-# Make sure serial device has permissions
-sudo chmod 777 /dev/ttyACM0
-
 # Launch prind in detached mode
 cd ~/prind
 sudo DOCKER_HOST=unix:///data/docker/run/docker.sock docker-compose --profile mainsail --profile klipperscreen up -d
