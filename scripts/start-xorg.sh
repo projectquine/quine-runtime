fc98898478338c9113458ba8660ce0820bb6bc2f
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-03-20 11:44:55 +0100

    launch termux-x11 app with xorg startup

--- scripts/start-xorg.sh
+++ scripts/start-xorg.sh
@@ -5,4 +5,5 @@ echo "Staring X11 server"
 Xvfb :0 -ac &
 DISPLAY=:0 termux-x11 &
 
-# TODO: use `am start activity` to launch the x11 app and bring it to the foreground
+# launch the x11 app. This will only bring it to the foreground if termux is on screen when it runs.
+am start -n com.termux.x11/.MainActivity
