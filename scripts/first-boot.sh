d669a2148f7b88a4855500e355f8e85b55782eb3
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-03-20 18:00:32 +0100

    remove need for sudo in first-boot.sh

--- scripts/first-boot.sh
+++ scripts/first-boot.sh
@@ -1,17 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Check if we have sudo privileges yet
-if [[ $(sudo ls 2>&1) == *"Permission denied"* ]]; then
-    echo "You don't have sudo privileges."
-    echo "Please grant them in the Magisk app!"
-    # Keep trying until user gets sudo privileges
-    while [[ $(sudo ls 2>&1) == *"Permission denied"* ]]; do
-        sleep 1
-    done
-fi
-# Once the user has sudo privileges
-echo "You now have sudo privileges."
-
 write_start_sshd() {
     echo '#!/data/data/com.termux/files/usr/bin/sh' > "$HOME"/.termux/boot/start-sshd
     echo termux-wake-lock >> "$HOME"/.termux/boot/start-sshd
@@ -40,20 +28,39 @@ write_boot_serial_watcher() {
 write_boot_quine() {
     echo '#!/data/data/com.termux/files/usr/bin/sh' > "$HOME"/.termux/boot/boot-quine
     echo exec 2>&1 >> "$HOME"/.termux/boot/boot-quine
-    echo exec $PREFIX/opt/quine/scripts/start-prind.sh >> "$HOME"/.termux/boot/boot-quine
+    echo "exec /data/data/com.termux/files/usr/opt/quine/scripts/start-prind.sh 2>&1 | tee -a $PREFIX/var/log/prind.log" >> "$HOME"/.termux/boot/boot-quine
 }
 
 # Create the start-services script in ~/.termux/boot to launch all other services using runit
-# TODO: do this without sudo, based on : https://github.com/termux/termux-packages/discussions/8292#discussioncomment-5102555
-sudo pm list packages | grep com.termux.boot >/dev/null && export a=1 || export a=0  
-if [ "$a" -eq 1 ]; then
-        mkdir -p "$HOME"/.termux/boot/                  
-        write_start_sshd
-        write_boot_dockerd
-        write_boot_serial_watcher
-        write_boot_xorg
-        write_boot_quine                                                       
+# based on : https://github.com/termux/termux-packages/discussions/8292#discussioncomment-5102555
+# Get the list of packages
+packages="$(pm list packages 2>&1 </dev/null)"
+
+# Check if the string 'com.termux.boot' is present in the packages list
+if echo "$packages" | grep -q "com.termux.boot"; then
+    echo "com.termux.boot package is installed"
+    mkdir -p "$HOME"/.termux/boot/                  
+    write_start_sshd
+    write_boot_dockerd
+    write_boot_serial_watcher
+    write_boot_xorg
+    write_boot_quine   
+else
+    echo "Termux:boot package is not installed, Please install it and reboot!"
+    exit 1
 fi
+
+# Check if we have sudo privileges yet
+if [[ $(sudo ls 2>&1) == *"Permission denied"* ]]; then
+    echo "You don't have sudo privileges."
+    echo "Please grant them in the Magisk app!"
+    # Keep trying until user gets sudo privileges
+    while [[ $(sudo ls 2>&1) == *"Permission denied"* ]]; do
+        sleep 1
+    done
+fi
+# Once the user has sudo privileges
+echo "You now have sudo privileges."
 
 # Check if we are connected to the internet for the install
 echo "Checking for internet access..."
@@ -83,4 +90,5 @@ rm "$PREFIX"/etc/profile.d/first-boot.sh > /dev/null
 
 # Reboot once everything is installed
 echo "Rebooting system to start system"
+sleep 5
 sudo /system/bin/reboot
