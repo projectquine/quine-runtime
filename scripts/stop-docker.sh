90c6da1928a91a1b8046742d09346e6849fd92d8
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-03-20 18:00:49 +0100

    use docker socket to test for stop-docker.sh

--- scripts/stop-docker.sh
+++ scripts/stop-docker.sh
@@ -1,21 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
-if [ sudo test -f /data/docker/run/docker.pid ]; then
+docker_res=$(sudo curl -f -s  "Content-Type: application/json" --unix-socket /data/docker/run/docker.sock http://localhost/_ping)
+echo $docker_res 
+if [ $docker_res == "OK" ]; then
+  echo "Found running docker instance"
   # Get the process IDs for all Docker-related processes
   pids=$(sudo pgrep -f docker)
-
   # Send SIGTERM to all the Docker-related processes
   sudo kill -s SIGTERM $pids
-
   # Wait for 5 seconds
   sleep 5
-
-  # Check if the docker.pid file has been removed
-  if [ -f /data/docker/run/docker.pid ]; then
-    # Send SIGKILL to all the Docker-related processes
-    sudo kill -s SIGKILL $pids
-
-    # Remove the docker.pid file
-    sudo rm /data/docker/run/docker.pid
-  fi
+  # Send SIGKILL to all the Docker-related processes
+  sudo kill -s SIGKILL $pids
+  # Remove the docker.pid file
+  sudo rm /data/docker/run/docker.pid
+else
+  echo "No Docker instance found running, so doing nothing!"
 fi
