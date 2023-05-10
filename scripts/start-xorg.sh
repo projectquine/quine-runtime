0f4bbad7ad0ba3de1b880dcac4f9e67e021a7246
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-05-10 10:43:52 +0200

    start-xorg.sh: don't launch x11 app, quinelauncher does it

--- scripts/start-xorg.sh
+++ scripts/start-xorg.sh
@@ -5,5 +5,3 @@ echo "Staring X11 server"
 Xvfb :0 -ac &
 DISPLAY=:0 termux-x11 &
 
-# launch the x11 app. This will only bring it to the foreground if termux is on screen when it runs.
-am start -n com.termux.x11/.MainActivity
